f0500e2139820f5e539014b97be213f1206deb6d
LaaZa <jesse.laatu@gmail.com>
2023-04-14 00:00:57 +0300

    Sentiment to be run async

--- plugins/chat/sentiment.py
+++ plugins/chat/sentiment.py
@@ -10,8 +10,8 @@ class Sentiment:
         self.classifier('preload')
         Globals.log.info('Completed Loading Sentiment analysis model.')
 
-    def emotion(self, text, limit):
-        prediction = self.classifier(text)
+    async def emotion(self, text, limit):
+        prediction = await Globals.disco.loop.run_in_executor(None, self.classifier, text)
         Globals.log.debug(f'Sentiment: {prediction}')
         try:
             if prediction[0][0]['score'] >= limit:
